49c73767404d359519f249b085e316a513b3a189
Rajdeep <rajdeepdas.india@gmail.com>
2019-10-10 18:59:41 +0530

    first build commit

--- src/test/java/com/webmonere/schedulerservice/SchedulerserviceApplicationTests.java
+++ src/test/java/com/webmonere/schedulerservice/SchedulerserviceApplicationTests.java
@@ -2,6 +2,7 @@ package com.webmonere.schedulerservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +10,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SchedulerserviceApplicationTests {
 
+	@Value("${http-stat-service-url}")
+	private String HTTP_STAT_SERVICE_URL;
 	@Test
 	public void contextLoads() {
+		System.out.println(HTTP_STAT_SERVICE_URL);
 	}
 
 }
